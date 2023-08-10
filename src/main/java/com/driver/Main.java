bb99c4060484a4357b9c7dd6f0b5af8c1754595e
Ahinsak Raj <ahinsakraj@gmail.com>
2023-08-10 11:23:49 +0530

    debug one

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,22 @@
 package com.driver;
 
 public class Main {
+    static class Product {
+        public Product() {
+        }
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
 
@@ -12,20 +28,4 @@ public class Main {
         System.out.println(f3);
     }
 
-}
-class Product {
-    public Product() {
-    }
-
-    public int product(int x, int y) {
-        return x * y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-
-    public double product(double x, double y) {
-        return x * y;
-    }
 }
