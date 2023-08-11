17b17937e31393aef4845f398590d07593ab87e1
Ahinsak Raj <ahinsakraj@gmail.com>
2023-08-12 00:01:25 +0530

    debug three

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,6 @@ package com.driver;
 
 public class Main {
     static class Product {
-        public Product() {
-        }
 
         public int product(int x, int y) {
             return x * y;
@@ -24,12 +22,6 @@ public class Main {
         p.product(3, 5, 4);
         p.product(4.5 , 5.5);
 
-//        int f1 = p.product(3, 5);
-//        int f2 = p.product(3, 5, 4);
-//        double f3 = p.product(4.5 , 5.5);
-//        System.out.println(f1);
-//        System.out.println(f2);
-//        System.out.println(f3);
     }
 
 }
